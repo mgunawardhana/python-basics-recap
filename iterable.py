eb2768f0202eab7e8e0b2af97e8cc49ce4fd3848
mgunawardhana <mrgunawardhana27368@gmail.com>
2025-01-01 22:24:13 +0530

    simple test

---
+++ iterable.py
@@ -0,0 +1,15 @@
+# print(type(5))
+# print(type(range(5)))
+#
+# for num in range(5):
+#     print(num)
+#
+# for val in "Maneesha":
+#     print(val)
+
+count = 0
+for number in range(1,10):
+    if number % 2 == 0:
+        count += 1
+        print(number)
+print(f"We have {count} even numbers")
